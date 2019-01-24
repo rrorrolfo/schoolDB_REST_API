03e20af410febf207998a7d2e62e339ac6560e1f
Rodolfo Perez <rodolfobeste@gmail.com>
2019-01-25 01:10:28 +0200

    Added functionality for getting all courses  and  getting one course by ID

--- routes/course.js
+++ routes/course.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { Course } = require("../models");
 
 // Returns a list of courses - including the user that owns each course
-router.get("/courses", (req, res) => {
-    
+router.get("/courses", (req, res, next) => {
+    Course.find({}, null, {sort: {title: 1}}, (err, courses) => {
+        if(err) {
+            return next(err);
+        } else {
+            res.status(200);
+            res.json(courses);
+        }
+    });
 });
 
 //Returns a specific course - including the user that owns the course
-router.get("/courses/:id", (req, res) => {
-    
+router.get("/courses/:cID", (req, res, next) => {
+    const course_to_find = req.params.cID;
+
+    Course.findById(course_to_find, (err, course) => {
+        if(err) {
+            return next(err);
+        } else {
+            res.status(200);
+            res.json(course);
+        }
+    });
+
 });
 
 // Creates a course, sets the Location header to the URI for the course, and returns no content
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models");
+const { check, validationResult } = require('express-validator/check');
 
 // returns the currently authenticated user
 router.get("/users", (req, res) => {
@@ -10,15 +11,16 @@ router.get("/users", (req, res) => {
 
 // Creates a user, sets the "Location" header to "/" and returns no content
 router.post("/users", (req, res, next) => {
-    
+
+    // Creates a nuew user in the DB 
     const user = new User(req.body);
     
     user.save( (err, user) => {
         if (err) {
             return next(err);
         } else {
+            res.location("/");
             res.status(201);
-            res.json(user);
         }
     });
 
